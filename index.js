59fbb1d24e602e129fe4fb6039b246ae4fa2be15
boboxiaodd <iam386@gmail.com>
2024-01-09 19:19:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-
 import { NativeModules } from 'react-native';
 import { requireNativeComponent } from 'react-native';
+import React, {Component} from 'react';
+const {AliveHelper} = NativeModules;
+const NTESRNLiveDetectView =  requireNativeComponent('NTESRNLiveDetect');
 
-export default NativeModules.AliveHelper;
-module.exports = requireNativeComponent('NTESRNLiveDetect');
+export const NTESRNLiveDetect = (props) => {
+    return (<NTESRNLiveDetectView {...props}/>);
+}
+export default AliveHelper;
